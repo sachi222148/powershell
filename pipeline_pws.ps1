2c2d02abb61094083208db21eff98cd3f64d8f47
ashish <ashish222148@gmail.com>
2025-09-02 08:50:31 +0530

    to show how the multiple pipes works in a single line

---
+++ pipeline_pws.ps1
@@ -0,0 +1,4 @@
+﻿# output of left commadn/expression will work as input of right command
+Get-Service -Name BITS | Start-Service
+
+Get-Service | Select-Object DisplayName,Status | Format-Table -AutoSize | Out-File "C:\temp\pipeline.txt" -Append
